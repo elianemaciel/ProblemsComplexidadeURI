68fcab04d2ef98de32bfdf0d81aa121edf1c363e
Eliane Isadora Faveron Maciel <eliane.faveron@gmail.com>
2019-06-25 21:33:42 -0300

    ajustes

--- 1798/main.py
+++ 1798/main.py
@@ -1,55 +1,65 @@
 # -*- coding: utf-8 -*-
+from functools import lru_cache
+import sys
+sys.setrecursionlimit(10000)
+
 
 NINF = -9999999
-dp = [[-1 for i in range(2005)] for i in range(1005)]
+dp = []
 num = 0
 valor = []
 comprimento = []
 
+@lru_cache(maxsize=None, typed=False)
 def solve(i, tamanho):
     global NINF, dp, num, valor, comprimento
-    print i, tamanho
-    if i == num:
+
+    if i == num or tamanho == 0:
         return 0
-    if tamanho == 0:
-        return 0
-    if tamanho <= 0:
+   
+    if tamanho < 0:
         return NINF
-
-    ans = dp[i][tamanho]
-    if ans == -1:
-        ans = max(solve(i + 1, tamanho), valor[i] + solve(i, tamanho - comprimento[i]))
-	return ans
-
-      if (id >= a.size()) return 0;
-  if (t <= 0) return 0;
-  if (dp[t][id] != -1) return dp[t][id];
-  int best = go(a, t, id + 1);
-  if (t >= a[id].len)
-    best = max(best, go(a, t - a[id].len, id) + a[id].cost);
-return dp[t][id] = best;
+    
+    if dp[i][tamanho] != -1:
+        return dp[i][tamanho]
+    if comprimento[i] > tamanho:
+        dp[i][tamanho] = solve(i+1, tamanho)
+        return dp[i][tamanho]
+    
+    dp[i][tamanho] = max(
+        solve(i + 1, tamanho),
+        valor[i] + solve(i, tamanho - comprimento[i])
+    )
+    return dp[i][tamanho]
 
 
 def main():
     global NINF, dp, num, valor, comprimento
-
-    # import ipdb; ipdb.set_trace()
-    import sys
-    sys.stdin = open('teste.txt')
-    line = raw_input().split()
-    num = int(line[0])
-    tam = int(line[1])
+    
     result = []
 
-    for i in range(num):
-        lines = raw_input().split()
-        comprimento.insert(i, int(lines[0]))
-        valor.insert(i, int(lines[0]))
+    import sys
+    sys.stdin = open('teste.txt')
+    
+    while True:
+        try:
+            line = input().split()
+        except EOFError:
+            break 
+        num = int(line[0])
+        tam = int(line[1])
+        comprimento = []
+        valor = []
+        for i in range(num):
+            lines = input().split()
+            comprimento.insert(i, int(lines[0]))
+            valor.insert(i, int(lines[1]))
 
-    dp = [[-1 for i in range(2005)] for i in range(1005)]
+        dp = [[-1 for i in range(2005)] for i in range(1005)]
 
-
-    solved = solve(0, tam)
-    print solved
+        result.append(solve(0, tam))
+    
+    for i in result:
+        print(i)
 
 main()
